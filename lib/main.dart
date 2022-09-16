3c20d81e04b5f26a01c995bd20e1f57b79814dfe
ItdaTeam <87175741+ItdaTeam@users.noreply.github.com>
2022-09-16 17:03:14 +0900

    ㄱ 4개 추가

--- lib/main.dart
+++ lib/main.dart
@@ -104,6 +104,12 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
 
             Text('ㄱ', style: Theme.of(context).textTheme.headline4,),
+            Text('ㄱ', style: Theme.of(context).textTheme.headline4,),
+            Text('ㄱ', style: Theme.of(context).textTheme.headline4,),
+            Text('ㄱ', style: Theme.of(context).textTheme.headline4,),
+
+
+
             Text('ㄴ', style: Theme.of(context).textTheme.headline4,),
             Text('ㄷ', style: Theme.of(context).textTheme.headline4,),
             Text('ㄹ', style: Theme.of(context).textTheme.headline4,),
